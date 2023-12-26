dc50734e04ab6f9bf261322bff25012e8a954cd9
hefengbao <754582231@qq.com>
2023-12-26 21:15:40 +0800

    重命名

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Authenticatable
+class AdminUser extends Authenticatable
 {
     use HasFactory;
 
--- config/auth.php
+++ config/auth.php
@@ -72,7 +72,7 @@ return [
 
         'admins' => [
             'driver' => 'eloquent',
-            'model' => App\Models\Admin::class,
+            'model' => App\Models\AdminUser::class,
         ],
 
         // 'users' => [
